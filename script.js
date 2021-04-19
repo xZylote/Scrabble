0ef3ef540a15cbc3668b8f437f25455b06364781
Luca Bosch <luca.bosch@uni.kn>
2021-04-19 17:36:23 +0200

    Only legal moves

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ var firstMove = true;
 function allowDrop(e) {
     e.preventDefault();
 }
-  
+
 function drag(e) {
     e.dataTransfer.setData("text", e.target.id);
     draggedOrigin = e.target.parentElement;
 }
-  
+
 function drop(e) {
-    e.preventDefault(); 
+    e.preventDefault();
     changedFields = changedFields.filter(item => item !== parseInt(draggedOrigin.id))
     draggedOrigin.setAttribute("ondragover", "allowDrop(event)")
     var data = e.dataTransfer.getData("text");
-    if(e.target.id != data){
-    e.target.appendChild(document.getElementById(data));
-    changedFields.push(parseInt(e.target.id));
-    e.target.ondragover = "";
+    if (e.target.id != data) {
+        e.target.appendChild(document.getElementById(data));
+        changedFields.push(parseInt(e.target.id));
+        e.target.ondragover = "";
     }
     console.log(changedFields)
     checkvalid();
@@ -34,12 +34,11 @@ function returnLetter(e) {
 }
 
 function done() {
-    if(firstMove){
-        firstMove =false;
+    if (firstMove) {
+        firstMove = false;
     }
     var word = "";
-    changedFields.sort()
-    for (item of changedFields){
+    for (item of changedFields) {
         document.getElementById(item).childNodes[0].setAttribute("draggable", "false")
         document.getElementById(item).childNodes[0].setAttribute("onclick", "")
         word += document.getElementById(item).childNodes[0].innerHTML
@@ -47,7 +46,7 @@ function done() {
     word = word.replace(/(\r\n|\n|\r)/gm, "").replace(/\s/g, "");
     console.log(word)
     //CHECK DICT
-    for (item of changedFields){
+    for (item of changedFields) {
         board[item] = document.getElementById(item).childNodes[0].innerHTML.replace(/(\r\n|\n|\r)/gm, "").replace(/\s/g, "");
     }
     console.log(board);
@@ -55,38 +54,76 @@ function done() {
     checkvalid();
 }
 function checkvalid() {
-
+    changedFields.sort((a, b) => a - b);
     document.getElementById("donebtn").disabled = true
-    if (changedFields.length != 0){
-    var row = true;
-    var column = true;
-    var numberRow = Math.floor(changedFields[0]/15);
-    for (item of changedFields){
-        if (!(item >= (numberRow*15) && item < (numberRow+1)*15)){
-        row = false;
-        }
-    }
+    if (changedFields.length != 0) {
 
-    for (item of changedFields){
-        if (item % 15 != changedFields[0] % 15){
-            column = false;
-        }
-    }
-
-    console.log(row, column)
-    if(row || column){
-        if(firstMove && changedFields.includes(112) ){
-            document.getElementById("donebtn").disabled = false
-        } else {
-            for (item of changedFields){
-                if (board[item-1] || board[item+1] || board[item-15] || board[item+15]){
-                    document.getElementById("donebtn").disabled = false
-                } 
+        var row = true;
+        var column = true;
+        var numberRow = Math.floor(changedFields[0] / 15);
+        for (item of changedFields) {
+            if (!(item >= (numberRow * 15) && item < (numberRow + 1) * 15)) {
+                row = false;
             }
-            
         }
 
-    }
+        for (item of changedFields) {
+            if (item % 15 != changedFields[0] % 15) {
+                column = false;
+            }
+        }
+
+        console.log(row, column)
+        if (row || column) {
+            if (firstMove && changedFields.includes(112)) {
+                var adjacent = true
+                for (item of changedFields) {
+                    if (!(changedFields.includes(item + 1) || changedFields.includes(item - 1) || changedFields.includes(item + 15) || changedFields.includes(item - 15))) {
+                        adjacent = false
+                    }
+                }
+                if (adjacent) {
+                    document.getElementById("donebtn").disabled = false
+                }
+            } else {
+                if (changedFields.length == 1) {
+                    if (board[changedFields[0] - 1] != null || board[changedFields[0] + 1] != null || board[changedFields[0] - 15] != null || board[changedFields[0] + 15] != null) {
+                        document.getElementById("donebtn").disabled = false
+                    }
+                } else {
+                    var connected = false
+                    var adjacent = true;
+                    if (row && !column) {
+                        for (var i = 0; i < changedFields.length; i++) {
+                            if (!((board[changedFields[i] + 1] != null || changedFields.includes(changedFields[i] + 1)) || (board[changedFields[i] - 1] != null || changedFields.includes(changedFields[i] - 1)))) {
+                                adjacent = false
+                            }
+                        }
+
+                    } else if (column && !row) {
+                        var adjacent = true;
+                        for (var i = 0; i < changedFields.length; i++) {
+                            if (!((board[changedFields[i] + 15] != null || changedFields.includes(changedFields[i] + 15)) || (board[changedFields[i] - 15] != null || changedFields.includes(changedFields[i] - 15)))) {
+                                adjacent = false
+                            }
+                        }
+
+                    }
+                    else {
+                        alert("something went wrong")
+                    }
+                    for (item of changedFields) {
+                        if (board[item + 1] != null || board[item - 1] != null || board[item + 15] != null || board[item - 15] != null) {
+                            connected = true;
+                        }
+                    }
+                    if (adjacent && connected) {
+                        document.getElementById("donebtn").disabled = false
+                    }
+                }
+            }
+
+        }
     }
 
 }
