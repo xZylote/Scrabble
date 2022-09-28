19ead1a85acd52ea29923be236e1fe20db1a65f4
Leon Stier <Lms@gnadenweiler.de>
2022-09-28 19:20:31 +0200

    Wird schon alles stimmen

--- server/server.js
+++ server/server.js
@@ -4,11 +4,12 @@ const io = require('socket.io')(http, {
     cors: { orgin: "*" }
 });
 
+var index = 1
 var players = []
 var board = new Array(225)
 var bag = ["E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "E_1", "N_1", "N_1", "N_1", "N_1", "N_1", "N_1", "N_1", "N_1", "N_1", "S_1", "S_1", "S_1", "S_1", "S_1", "S_1", "S_1", "I_1", "I_1", "I_1", "I_1", "I_1", "I_1", "R_1", "R_1", "R_1", "R_1", "R_1", "R_1", "T_1", "T_1", "T_1", "T_1", "T_1", "T_1", "U_1", "U_1", "U_1", "U_1", "U_1", "U_1", "A_1", "A_1", "A_1", "A_1", "A_1", "D_1", "D_1", "D_1", "D_1", "H_2", "H_2", "H_2", "H_2", "M_3", "M_3", "M_3", "M_3", "G_2", "G_2", "G_2", "L_2", "L_2", "L_2", "O_2", "O_2", "O_2", "B_3", "B_3", "C_4", "C_4", "F_4", "F_4", "K_4", "K_4", "W_3", "Z_3", "P_4", "J_6", "V_6", "X_8", "Q_10", "Y_10"]
 var scores = []
-var turn = "no User"
+var turn = 0
 
 
 io.on('connection', (socket) => {
@@ -18,15 +19,20 @@ io.on('connection', (socket) => {
     });
 
     socket.on('playerconnect', (value) => {
-        if (players.includes(value)) {
+        var player = {
+            socket : socket.id,
+            index : index++,
+            name : value
+        }
+        if (players.includes(player)) {
             console.log("the retard logged in twice")
         }
-        console.log("Player " + value + " connected")
-        players.push(value)
+        console.log("Player " + player + " connected")
+        players.push(player)
         if (players.length == 1) {
             turn = players[0]
         }
-        io.emit('newPlayerAnnouncement', value)
+        io.emit('newPlayerAnnouncement', player)
         io.emit('playerListUpdate', players);
         io.emit('turnResponse', turn);
     });
@@ -50,26 +56,22 @@ io.on('connection', (socket) => {
         io.emit('scoreResponse', scores);
     });
 
+    socket.on('clearScores', () => {
+        players = []
+        io.emit('playerListUpdate', players);
+    })
+
     socket.on('done', (value) => {
-        if (players.indexOf(value) + 1 != players.length) {
-            turn = players[players.indexOf(value) + 1];
-        }
-        else {
-            turn = players[0]
-        }
-        io.emit('turnResponse', turn);
+        turn = (turn + 1) % players.length
+        io.emit('turnResponse', players[turn].name);
     });
 
     socket.on('setBag', (value) => {
         bag = value
     });
 
-    socket.on('dc', (value) => {
-        const index = players.indexOf(value);
-        if (index > -1) {
-            players.splice(index, 1);
-        }
-        console.log("Player " + value + " disconnected")
+    socket.on('disconnect', (username) => {
+        players.filter(function(player) { return player.name == username; });
         io.emit('playerListUpdate', players);
     });
 
